e1c084c1fa384e0481a2ee63c40ed62310b859d4
suy0n9 <suy01v9@gmail.com>
2018-02-21 01:54:53 +0900

    return obj

--- createEvent.js
+++ createEvent.js
@@ -12,20 +12,25 @@ function createEvent(labelName, keyword) {
     
     for (var j = 0; j < messages.length; j++) {
       var body = messages[j].getBody()
-      //Logger.log(body)
       var subject = messages[j].getSubject()
       
       if (subject.indexOf('TOHO CINEMAS') >0){
-        getTohoData(body)
+        var result = getTohoData(body)
       } else if (subject.indexOf('ユナイテッド・シネマ グループ') >0){
-        getUnitedData(body)
+        var result = getUnitedData(body)
       }
+      
+      Logger.log('========')
+      Logger.log(result.theater)
+      Logger.log(result.buy_num)
+      Logger.log(result.title)
+      Logger.log('start_time:' + result.start)
+      Logger.log('end_time:' + result.end)
 
-      //var THEATER. Use the variable defined in config.gs
-      //insertEventToCalendar(title, start, end, buy_num, THEATER)
+      insertEventToCalendar(result.title, result.theater, result.start, result.end, result.buy_num)
     }
     var thread = threads[i]
-    //thread.removeLabel(GmailApp.getUserLabelByName(label))
+    thread.removeLabel(GmailApp.getUserLabelByName(label))
   }
 }
 
@@ -36,23 +41,19 @@ function getTohoData(body) {
   ■上映日 Date  2017/12/24　■時間 Time  20:30～
   ■映画名称 Movie  （字）XXXXXXX
   */
-  var theater = fetchData(body, '■映画館 Theater', '<br>').trim().split(/\s+/)[0]
-  var buy_num = fetchData(body, '■購入番号', '<br>')
-  var title = fetchData(body,'■映画名称 Movie', '<br>').trim()
+  var obj = new Object()
+
+  obj.theater = fetchData(body, '■映画館 Theater', '<br>').trim().split(/\s+/)[0]
+  obj.buy_num = fetchData(body, '■購入番号', '<br>')
+  obj.title = fetchData(body,'■映画名称 Movie', '<br>').trim()
+  
   var date = fetchData(body, '■上映日 Date', '<br>')
   var date_arg = date.match(/\d+\/\d+\/\d+/)
   var start_time = date.split('■時間 Time')[1].match(/\d{2}:+\d{2}/)
+  obj.start = new Date(date_arg + ' ' + start_time)
+  obj.end = new Date(+obj.start + 120*60*1000)
 
-  var start = new Date(date_arg + ' ' + start_time)
-  var end = new Date(+start + 120*60*1000)
-
-  Logger.log(theater)
-  Logger.log(buy_num)
-  Logger.log(title)
-  Logger.log(date_arg)
-  //Logger.log(time)
-  Logger.log('start_time:' + start)
-  Logger.log('end_time:' + end)
+  return obj
 }
 
 function getUnitedData(body) {
@@ -63,25 +64,22 @@ function getUnitedData(body) {
   鑑賞日：2018/01/19（金）
   上映時間：21:30～23:35
   */
+  var obj = new Object()
+  
   var theater_place = fetchData(body, '劇場：', '<br>')
-  var theater = 'ユナイテッド・シネマ' + theater_place
-  var buy_num = fetchData(body, 'お客さまの購入番号は', '<br>')
-  var title = fetchData(body,'作品名：', '<br>')
+  obj.theater = 'ユナイテッド・シネマ' + theater_place
+  obj.buy_num = fetchData(body, 'お客さまの購入番号は', '<br>')
+  obj.title = fetchData(body,'作品名：', '<br>')
   var date = fetchData(body, '鑑賞日：', '<br>')
   var time = fetchData(body, '上映時間：', '<br>')
   var date_arg = date.match(/\d+\/\d+\/\d+/)
   var start_time = time.split('～')[0]
   var end_time = time.split('～')[1]
 
-  var start = new Date(date_arg + ' ' + start_time)
-  var end = new Date(date_arg + ' ' + end_time)
-
-  Logger.log(theater)
-  Logger.log(buy_num)
-  Logger.log(title)
-  Logger.log(date_arg)
-  Logger.log(start)
-  Logger.log(end)
+  obj.start = new Date(date_arg + ' ' + start_time)
+  obj.end = new Date(date_arg + ' ' + end_time)
+  
+  return obj
 }
 
 function fetchData(str, pre, suf) {
@@ -98,11 +96,11 @@ function fetchData(str, pre, suf) {
   }
 }
 
-function insertEventToCalendar(title, start, end, text, THEATER) {
+function insertEventToCalendar(title, theater, start, end, text) {
   //var calendarId.  Use the variable defined in config.gs
   var event = {
     summary: title,
-    location: THEATER,
+    location: theater,
     description: text,
     start: {
       dateTime: start.toISOString()
