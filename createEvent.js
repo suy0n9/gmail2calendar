76438204819e10e98e2ee35de3ad279fe5019d2d
suy0n9 <suy01v9@gmail.com>
2018-02-22 00:47:53 +0900

    fix RegExp and function

--- createEvent.js
+++ createEvent.js
@@ -1,10 +1,10 @@
 function main() {
-  createEvent('toho', 'TOHO CINEMAS')
-  createEvent('united', 'ユナイテッド・シネマ グループ')
+  console.info('=== Start ===')
+  createEvent('movie')
+  console.info('=== End ===')
 }
 
-function createEvent(labelName, keyword) {
-  console.info('=== Start createEvent ===')
+function createEvent(labelName) {
   var label = labelName
   var threads = GmailApp.getUserLabelByName(label).getThreads()
   
@@ -21,19 +21,24 @@ function createEvent(labelName, keyword) {
         var result = getUnitedData(body)
       }
       
-      Logger.log('========')
-      Logger.log(result.theater)
-      Logger.log(result.buy_num)
-      Logger.log(result.title)
-      Logger.log('start_time:' + result.start)
-      Logger.log('end_time:' + result.end)
-
-      insertEventToCalendar(result.title, result.theater, result.start, result.end, result.buy_num)
+      var calendar = CalendarApp.getDefaultCalendar()
+      var registed_events = calendar.getEvents(result.start, result.end, {search: result.title})
+      
+      if (registed_events.length == 0) {
+        Logger.log('--------------------------')
+        Logger.log('theater: ' + result.theater)
+        Logger.log('Confirmation Number: ' + result.buy_num)
+        Logger.log('title: ' + result.title)
+        Logger.log('start_time: ' + result.start)
+        Logger.log('end_time: ' + result.end)
+        
+        insertEventToCalendar(result.title, result.theater, result.start, result.end, result.buy_num)
+      } else {
+        Logger.log('alredy exist event: ' + result.title)
+      }
+      
     }
-    var thread = threads[i]
-    thread.removeLabel(GmailApp.getUserLabelByName(label))
   }
-  console.info('=== End createEvent ===')
 }
 
 function getTohoData(body) {
@@ -45,9 +50,9 @@ function getTohoData(body) {
   */
   var obj = new Object()
 
+  obj.buy_num = body.match(/Confirmation Number\s*(\d{4})/)[1]
   obj.theater = fetchData(body, '■映画館 Theater', '<br>').trim().split(/\s+/)[0]
-  obj.buy_num = fetchData(body, '■購入番号', '<br>')
-  obj.title = fetchData(body,'■映画名称 Movie', '<br>').trim()
+  obj.title = fetchData(body,'■映画名称 Movie', '/ English<br>').trim()
   
   var date = fetchData(body, '■上映日 Date', '<br>')
   var date_arg = date.match(/\d+\/\d+\/\d+/)
@@ -70,7 +75,8 @@ function getUnitedData(body) {
   
   var theater_place = fetchData(body, '劇場：', '<br>')
   obj.theater = 'ユナイテッド・シネマ' + theater_place
-  obj.buy_num = fetchData(body, 'お客さまの購入番号は', '<br>')
+  obj.buy_num = body.match(/【(\d{4})】/)[1]
+  
   obj.title = fetchData(body,'作品名：', '<br>')
   var date = fetchData(body, '鑑賞日：', '<br>')
   var time = fetchData(body, '上映時間：', '<br>')
@@ -87,25 +93,19 @@ function getUnitedData(body) {
 function fetchData(str, pre, suf) {
   var reg = new RegExp(pre + '.*?' + suf)
   var data = str.match(reg)[0]
-  
-  if (data.match(/購入番号/)) {
-    var data = data.replace(suf, '')
-    return data
-  } else {
-    var data = data.replace(pre, '')
-                   .replace(suf, '')
-    return data
-  }
+               .replace(pre, '')
+               .replace(suf, '')
+  return data
 }
 
-function insertEventToCalendar(title, theater, start, end, text) {
+function insertEventToCalendar(title, theater, start, end, buy_number) {
   console.info('=== Start insertEventToCalendar ===')
   
   //var calendarId.  Use the variable defined in config.gs
-  var event = {
+  var event_data = {
     summary: title,
     location: theater,
-    description: text,
+    description: 'Confirmation Number ' + buy_number,
     start: {
       dateTime: start.toISOString()
     },
@@ -115,8 +115,9 @@ function insertEventToCalendar(title, theater, start, end, text) {
     //PURPlE background. Use Calendar.Colors.get() for the full list.
     colorId: 3
   }
-  event = Calendar.Events.insert(event, calendarId)
-  
-  console.info('=== End insertEventToCalendar ===')
+  var event = Calendar.Events.insert(event_data, calendarId)
+
+  console.log({message: 'Event registration: ' + title, initialData: event_data})
   Logger.log('Event ID: ' + event.getId())
+  console.info('=== End insertEventToCalendar ===')
 }
