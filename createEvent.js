e0694c9575bc3176a2637f80ef05ea8f7e6b27e6
suy0n9 <suy01v9@gmail.com>
2018-03-12 16:10:18 +0900

    Fix RegExp

--- createEvent.js
+++ createEvent.js
@@ -12,15 +12,16 @@ function createEvent(labelName) {
     var messages = threads[i].getMessages()
     
     for (var j = 0; j < messages.length; j++) {
-      var body = messages[j].getBody()
+      var body = messages[j].getPlainBody()
+      var splitedBody = body.split('\n')
       var subject = messages[j].getSubject()
-
-      if (subject.indexOf('TOHO CINEMAS') >0){
-        var result = getTohoData(body)
-      } else if (subject.indexOf('ユナイテッド・シネマ グループ') >0){
-        var result = getUnitedData(body)
-      }
       
+      if (subject.indexOf('TOHO CINEMAS') >0){
+        var result = getTohoData(splitedBody)
+      } else if (subject.indexOf('ユナイテッド・シネマ グループ') >0){
+        var result = getUnitedData(splitedBody)
+      }
+
       var calendar = CalendarApp.getDefaultCalendar()
       var registed_events = calendar.getEvents(result.start, result.end, {search: result.title})
       
@@ -41,7 +42,7 @@ function createEvent(labelName) {
   }
 }
 
-function getTohoData(body) {
+function getTohoData(splitedBody) {
   /*
   ■購入番号 Confirmation Number  XXXX　(TEL:XXX-XXX-XXX)
   ■映画館 Theater  XXXXX
@@ -49,21 +50,38 @@ function getTohoData(body) {
   ■映画名称 Movie  （字）XXXXXXX
   */
   var obj = new Object()
-
-  obj.buy_num = body.match(/Confirmation Number\s*(\d{4})/)[1]
-  obj.theater = fetchData(body, '■映画館 Theater', '<br>').trim().split(/\s+/)[0]
-  obj.title = fetchData(body,'■映画名称 Movie', '<br>').trim().replace(/\/ [A-Za-z]+/, '')
   
-  var date = fetchData(body, '■上映日 Date', '<br>')
-  var date_arg = date.match(/\d+\/\d+\/\d+/)
-  var start_time = date.split('■時間 Time')[1].match(/\d{2}:+\d{2}/)
-  obj.start = new Date(date_arg + ' ' + start_time)
-  obj.end = new Date(+obj.start + 120*60*1000)
+  for (var k = 0; k < splitedBody.length; k++) {  
+    var str = splitedBody[k]
+    
+    if (str.match(/Confirmation Number/)){
+      obj.buy_num = str.match(/Confirmation Number\s*(\d{4})/)[1]
+      Logger.log('+++++ Confirmation Number ' + obj.buy_num)
+    }
+    
+    if (str.match(/Theater/)){
+      obj.theater = str.match(/Theater\s*([^ ]+)/)[1]
+      Logger.log('+++++ theater name ' + obj.theater)
+    }
 
+    if (str.match(/Movie/)){
+      obj.title = str.match(/Movie\s(.*) \//)[1]
+      Logger.log('+++++ title ' + obj.title)
+    }
+    
+    if (str.match(/Date/)){
+      var date = str.match(/Date\s*(\d+\/\d+\/\d+)/)[1]
+      var start_time = str.match(/Time\s*(\d{2}:\d{2})/)[1]
+      obj.start = new Date(date + ' ' + start_time)
+      obj.end = new Date(+obj.start + 120*60*1000)
+      Logger.log('+++++ date ' + date)
+      Logger.log('+++++ time ' + obj.start + '~' + obj.end)
+    }
+  }
   return obj
 }
 
-function getUnitedData(body) {
+function getUnitedData(splitedBody) {
   /*
   ●作品・劇場・時間
   劇場：XXXXX
@@ -71,32 +89,44 @@ function getUnitedData(body) {
   鑑賞日：yyyy/mm/dd（金）
   上映時間：hh:mm～hh:mm
   */
+
   var obj = new Object()
   
-  var theater_place = fetchData(body, '劇場：', '<br>')
-  obj.theater = 'ユナイテッド・シネマ' + theater_place
-  obj.buy_num = body.match(/【(\d{4})】/)[1]
-
-  obj.title = fetchData(body,'作品名：', '<br>')
-  var date = fetchData(body, '鑑賞日：', '<br>')
-  var date_arg = date.match(/\d+\/\d+\/\d+/)
-  var time = fetchData(body, '上映時間：', '<br>')
-  var start_time = time.split('～')[0]
-  var end_time = time.split('～')[1]
-
-  obj.start = new Date(date_arg + ' ' + start_time)
-  obj.end = new Date(date_arg + ' ' + end_time)
-  
+  for (var k = 0; k < splitedBody.length; k++) {
+    var str = splitedBody[k]
+    
+    if (str.match(/劇場：/)){
+      var theater_place = str.match(/劇場：(.+)/)
+      obj.theater = 'ユナイテッド・シネマ' + theater_place[1]
+      Logger.log('+++++ theater name ' + obj.theater)
+    }
+    
+    if (str.match(/【(\d{4})】/)){
+      obj.buy_num = str.match(/【(\d{4})】/)[1]
+      Logger.log('+++++ buy num ' + obj.buy_num)
+    }
+    
+    if (str.match(/作品名：/)){
+      obj.title = str.match(/作品名：(.+)/)[1]
+      Logger.log('+++++ title ' + obj.title)
+    }
+    
+    if (str.match(/鑑賞日：/)){
+      var date = str.match(/鑑賞日：(\d+\/\d+\/\d+)/)[1]
+      Logger.log('+++++ date ' + date)
+    }
+    
+    if (str.match(/上映時間：/)){
+      var time = str.match(/上映時間：(\d{2}:\d{2})～(\d{2}:\d{2})/)
+      Logger.log('+++++ time ' + time)
+    }
+    
+    if (date !=null && time !=null) {
+      obj.start = new Date(date + ' ' + time[1])
+      obj.end = new Date(date + ' ' + time[2])
+    }
+  }
   return obj
-}
-
-function fetchData(str, pre, suf) {
-  var reg = new RegExp(pre + '.*?' + suf)
-  var data = str.match(reg)[0]
-                .replace(pre, '')
-                .replace(suf, '')
-                .replace('</span>','')
-  return data
 }
 
 function insertEventToCalendar(title, theater, start, end, buy_number) {
